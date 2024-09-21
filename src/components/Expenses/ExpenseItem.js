345ee46fd742fc05a1702c1f86ad4435868db647
Prathamesh Chavan <prathamchavan0013@gmail.com>
2024-09-21 23:19:32 +0530

    Adding form inputs

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,6 +7,7 @@ import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
   // function clickHandler() {}
   const [title, setTitle] = useState(props.title);
+  console.log("ExpenseItem Evaluated by React");
 
   // let title = props.title;
 
@@ -19,7 +20,7 @@ const ExpenseItem = (props) => {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
